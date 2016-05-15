1be8fdce3c8d7581d3283724e5ae91ea1b6c61ea
liangshanbo <2368713919@qq.com>
2016-05-15 23:09:48 +0800

    hoderplace解决，但是qq验证时有兼容性问题，

--- demo01/js/formjs.js
+++ demo01/js/formjs.js
@@ -45,11 +45,37 @@ var MyForm = React.createClass({
         newState[name]=event.target.value;
         this.setState(newState);
     },
+
+
     //倒计时方法
     interval:null,
     sleep:10,
     flog:true,
     clickHandle:function () {
+
+        var phonenumvalue1=this.state.phonenum;
+        var phonenumerror1 = '';
+        var phonenumstyle1="right";
+        if(!(/^1[3|4|5|7|8]\d{9}$/.test(phonenumvalue1))) {
+            phonenumerror1 = '请输入正确的手机号';
+            phonenumstyle1="error invalid" ;
+            phonenumvalue1="";
+            this.addPlaceholder(phonenumerror1,'phonenum');
+            this.setState({
+                phonenum:phonenumvalue1,
+                phonenumError: phonenumerror1,
+                phonenumStyle: phonenumstyle1
+            });
+            return;
+        }
+        this.setState({
+            phonenum:phonenumvalue1,
+            phonenumError: phonenumerror1,
+            phonenumStyle: phonenumstyle1
+        });
+
+
+
         if(!this.interval)
         {
             var text=   " (" + this.sleep-- +")秒后重新发送";
@@ -91,7 +117,63 @@ var MyForm = React.createClass({
             $p.find("input[type='text']").trigger("focusout");
         }
     },
+     checkphonenum:function(){
+         //电话验证
+         var phonenumvalue1 = this.state.phonenum;
+         var phonenumerror1 = '';
+         var phonenumstyle1="right";
+         if(!(/^1[3|4|5|7|8]\d{9}$/.test(phonenumvalue1))) {
+             phonenumerror1 = '请输入正确的手机号';
+             phonenumstyle1="error invalid" ;
+             phonenumvalue1="";
+             this.addPlaceholder(phonenumerror1,'phonenum');
+             if ( e && e.preventDefault )
+                 e.preventDefault();
+             //IE中阻止函数器默认动作的方式
+             else
+                 window.event.returnValue = false;
+//            return false;
+         }
+         this.setState({
+             phonenum:phonenumvalue1,
+             phonenumError: phonenumerror1,
+             phonenumStyle: phonenumstyle1
+         });
+    },
     handleSubmit:function (e) {
+        var qqvalue1=this.state.qq;
+        qqvalue1=qqvalue1.trim();
+        var qqerror1='';
+        var qqstyle1='right';
+
+        if(qqvalue1!=""){
+
+            if(qqvalue1.search(/^[1-9]\d{4,8}$/) !=-1){
+
+             }else{
+
+                qqerror1 = '请输入正确的QQ号';
+                qqstyle1="error invalid" ;
+                qqvalue1="";
+                this.addPlaceholder(qqerror1,'qq');
+                if ( e && e.preventDefault )
+                    e.preventDefault();
+                //IE中阻止函数器默认动作的方式
+                else
+                    window.event.returnValue = false;
+    //            return false;
+             }
+      }else{
+            alert("sdfsdf");
+            qqerror1 = 'QQ号';
+            this.addPlaceholder(qqerror1,'qq');
+    }
+        this.setState({
+            qq:qqvalue1,
+            qqError: qqerror1,
+            qqStyle:  qqstyle1
+        });
+
         //用户名验证
         var usernamevalue1 = this.state.username;
         var usererror1 = '';
@@ -99,7 +181,8 @@ var MyForm = React.createClass({
         if(usernamevalue1.trim()=='') {
             usererror1 = '请输入用户名';
             userstyle1="error invalid" ;
-            this.flog=false;
+            usernamevalue1="",
+
             this.addPlaceholder(usererror1,'username');
             if ( e && e.preventDefault )
                 e.preventDefault();
@@ -109,6 +192,7 @@ var MyForm = React.createClass({
 //            return false;
         }
         this.setState({
+            username:usernamevalue1,
             usernameError: usererror1,
             usernameStyle:  userstyle1
         });
@@ -117,6 +201,7 @@ var MyForm = React.createClass({
         var agencynameerror1 = '';
         var agencynamestyle1="right";
         if(agencynamevalue1.trim()=='') {
+            agencynamevalue1="",
             agencynameerror1 = '请输入机构名称';
             agencynamestyle1="error invalid" ;
             this.addPlaceholder(agencynameerror1,'agencyname');
@@ -128,6 +213,7 @@ var MyForm = React.createClass({
 //            return false;
         }
         this.setState({
+            agencyname:agencynamevalue1,
             agencynameError: agencynameerror1,
             agencynameStyle:agencynamestyle1
         });
@@ -152,6 +238,31 @@ var MyForm = React.createClass({
             phonenumError: phonenumerror1,
             phonenumStyle: phonenumstyle1
         });
+
+        //验证码验证
+        var verifycodevalue1 = this.state.verifycode;
+        var verifycodeerror1 = '';
+        var verifycodestyle1="right";
+        if(verifycodevalue1.trim()=='') {
+            verifycodevalue1="";
+            verifycodeerror1 = '请输入验证码';
+            verifycodestyle1="error invalid" ;
+            this.addPlaceholder(verifycodeerror1,'verifycode');
+            if ( e && e.preventDefault )
+                e.preventDefault();
+            //IE中阻止函数器默认动作的方式
+            else
+                window.event.returnValue = false;
+//            return false;
+        }
+        this.setState({
+            verifycode:verifycodevalue1,
+            verifycodeError: verifycodeerror1,
+            verifycodeStyle:verifycodestyle1
+        });
+
+
+
         // 公司邮箱验证
         var emailvalue1 = this.state.email;
         var emailerror1 = '';
@@ -161,7 +272,6 @@ var MyForm = React.createClass({
             emailvalue1="";
             emailstyle1="error invalid" ;
             this.addPlaceholder(emailerror1,'email');
-
             if ( e && e.preventDefault )
                 e.preventDefault();
             //IE中阻止函数器默认动作的方式
@@ -200,7 +310,7 @@ var MyForm = React.createClass({
 
                     </p>
                     <p className={this.state.qqStyle}>
-                        <input type='text' name='qq' id='qq' placeholder="QQ号码" value={this.state.qq}  onChange={this.handleChange.bind(this,"qq")} />
+                        <input type='text' name='qq' id='qq' placeholder={this.state.qqError} value={this.state.qq}  onChange={this.handleChange.bind(this,"qq")} />
                     </p>
                 </div>
                 <input type="submit" style={{width:"420px",height:"40px",marginTop:"20px", borderRadius:'5px'}} className="btn" value="提交"/>
